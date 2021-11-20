ce482da079a0c40cc04c163445a11445dd19f24c
Smruti Ranjan Badatya <35039502+Mr-LazyProgrammer@users.noreply.github.com>
2021-11-20 21:42:08 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,8 @@ let sec = document.querySelector('.sec')
 let d = new Date()
 function clock(){
     let d = new Date()
-    sec.style.transform =  `rotateZ(${d.getSeconds()*6}deg)`
-    min.style.transform =  `rotateZ(${d.getMinutes()*6 + d.getSeconds()*0.1}deg)`
-    hour.style.transform =  `rotateZ(${d.getHours()*30 + (d.getMinutes()*6 + d.getSeconds()*0.1)*0.5}deg)`
+    sec.style.transform =  `rotateZ(${d.getSeconds()*6 + 90}deg)`
+    min.style.transform =  `rotateZ(${d.getMinutes()*6 + 90}deg)`
+    hour.style.transform =  `rotateZ(${d.getHours()*30 + (d.getMinutes())*0.5 + 90}deg)`
 }
 setInterval(clock, 1000)
